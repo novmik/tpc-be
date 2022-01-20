ffc0123218fe459f7cab197e6a89bbf43f49cece
Mikhail <novmik@gmail.com>
2022-01-20 14:17:24 +0300

    Test

--- src/main/java/com/novmik/tpc/TreatmentPaymentCalculatorApplication.java
+++ src/main/java/com/novmik/tpc/TreatmentPaymentCalculatorApplication.java
@@ -35,7 +35,7 @@ public class TreatmentPaymentCalculatorApplication {
   public CorsFilter corsFilter() {
     final CorsConfiguration corsConfiguration = new CorsConfiguration();
     corsConfiguration.setAllowCredentials(true);
-    corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "http://novmik-tpc-fe.s3-website.eu-central-1.amazonaws.com"));
+    corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "http://tpc-env.eba-u2m7eyq6.eu-central-1.elasticbeanstalk.com/"));
     corsConfiguration.setAllowedHeaders(
         Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
             "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
