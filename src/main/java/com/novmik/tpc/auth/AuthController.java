251ba8c6ad5d33a320a56e96701ea7beb6cbca4a
Mikhail <novmik@gmail.com>
2022-01-20 16:27:28 +0300

    fix unused imports.

--- src/main/java/com/novmik/tpc/auth/AuthController.java
+++ src/main/java/com/novmik/tpc/auth/AuthController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
