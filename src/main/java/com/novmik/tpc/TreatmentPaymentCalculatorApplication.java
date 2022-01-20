a0ebdd41a7dfbab691400370970e97c2815fbd95
Mikhail <novmik@gmail.com>
2022-01-20 10:51:23 +0300

    Test

--- src/main/java/com/novmik/tpc/TreatmentPaymentCalculatorApplication.java
+++ src/main/java/com/novmik/tpc/TreatmentPaymentCalculatorApplication.java
@@ -25,4 +25,31 @@ public class TreatmentPaymentCalculatorApplication {
   public static void main(final String[] args) {
     SpringApplication.run(TreatmentPaymentCalculatorApplication.class, args);
   }
+
+  /**
+   * CORS configuration.
+
+   * @return CorsFilter
+   */
+  @Bean
+  public CorsFilter corsFilter() {
+    final CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowCredentials(true);
+    corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+    corsConfiguration.setAllowedHeaders(
+        Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+            "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+            "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+    corsConfiguration.setExposedHeaders(
+        Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+            "Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
+            "Access-Control-Allow-Credentials"));
+    corsConfiguration.setAllowedMethods(Arrays.asList(
+        "GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    final UrlBasedCorsConfigurationSource urlBasedCors =
+        new UrlBasedCorsConfigurationSource();
+    urlBasedCors.registerCorsConfiguration(
+        "/**", corsConfiguration);
+    return new CorsFilter(urlBasedCors);
+  }
 }
