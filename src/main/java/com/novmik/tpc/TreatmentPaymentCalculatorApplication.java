800c424e3bfa03c7cb88da044acc471b7444b028
Mikhail <novmik@gmail.com>
2022-01-20 16:12:43 +0300

    Test

--- src/main/java/com/novmik/tpc/TreatmentPaymentCalculatorApplication.java
+++ src/main/java/com/novmik/tpc/TreatmentPaymentCalculatorApplication.java
@@ -35,7 +35,7 @@ public class TreatmentPaymentCalculatorApplication {
   public CorsFilter corsFilter() {
     final CorsConfiguration corsConfiguration = new CorsConfiguration();
     corsConfiguration.setAllowCredentials(true);
-    corsConfiguration.setAllowedOrigins(List.of("**"));
+    corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
     corsConfiguration.setAllowedHeaders(
         Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
             "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
